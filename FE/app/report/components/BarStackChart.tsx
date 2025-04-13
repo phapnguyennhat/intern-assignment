50a81b9c314b2ce8068cb45acb62994cc2618f09
Nguyen Nhat Phap <nguyennhatphapbp@gmail.com>
2025-04-14 00:06:20 +0700

    fix: remove unused var

--- FE/app/report/components/BarStackChart.tsx
+++ FE/app/report/components/BarStackChart.tsx
@@ -1,12 +1,10 @@
 "use client"
 
-import { TrendingUp } from "lucide-react"
 import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
 
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
